3310d7ec11e7933e5a8d09c8693dff38cbc084bd
Malcolm-G-Biz <malcolm.githuka@bizhub.com.au>
2025-11-16 21:36:03 +0300

    Initialize project

---
+++ src/test/java/com/malcolm/about_me/AboutMeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malcolm.about_me;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AboutMeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
